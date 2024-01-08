d1ec1b0067c64878475fa7e1be56247563f2352d
RahulYavvari <88090422+RahulYavvari@users.noreply.github.com>
2024-01-08 19:26:49 +0530

    Update generateSoundAPI.js

--- generateSoundAPI.js
+++ generateSoundAPI.js
@@ -14,21 +14,25 @@ function getSoundAPI(frequency, intensity, duration, speakerSide) {
     }
 
     if (speakerSide === "left") {
-        dataR.fill(0);
+        dataR.fill(0); 
     } else if (speakerSide === "right") {
         dataL.fill(0);
     }
 
+    const panNode = audioContext.createStereoPanner();
+
     const source = audioContext.createBufferSource();
     source.buffer = buffer;
-    source.connect(audioContext.destination);
+    source.connect(panNode);
+    panNode.connect(audioContext.destination);
+
+    panNode.pan.setValueAtTime(speakerSide === "right" ? 1 : -1, audioContext.currentTime);
+
     source.start();
 
     setTimeout(() => source.stop(), duration * 1000);
 }
 
-
 const standard_audiometric_reference = [7, 6, 5, 0, -2, -2, -1, -2, -1, 1];
 const reference_frequency = [250, 500, 750, 1000, 1500, 2000, 3000, 4000, 6000, 8000];
 const reference_intensity_db = [-10, 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120];
-
